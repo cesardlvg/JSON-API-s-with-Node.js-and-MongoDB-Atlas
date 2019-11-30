360831ba910fe712dc0698cbc5df4af8a70ecd4a
Cesar <you@example.com>
2019-11-30 15:22:08 +0000

    Added front page html, css and jquery

--- app.js
+++ app.js
@@ -10,9 +10,12 @@ let todoRoutes = require('./routes/todos');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', function(req, res){
-	res.send("Hi The from the root route!")
+	res.sendFile("index.html");
 });
 
 
